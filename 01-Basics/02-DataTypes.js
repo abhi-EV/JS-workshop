2af6f8e3eac5bc8b2a71144c9a9423941c9b30b2
abhi-EV <abhishekabhishek7026@gmail.com>
2024-05-25 12:55:50 +0530

    dataTypes

---
+++ 01-Basics/02-DataTypes.js
@@ -0,0 +1,38 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello"); // it gives o/p in browser but we r in local using node.js here is a different syntax
+// console.log(2+2); console.log("hello")
+/* we should not write in same line even after writing semicolon we should always go with next line for good coding practice and code rediablity*/
+/*console.log(2+2); 
+console.log("hello");*/
+// follow like this one by one
+
+/*                           DATATYPES
+
+            // primitive
+      1.number => 2 to the power of 53 we have bigInt also
+      2.string => "" or ''
+      3.boolean => true or false
+      4.null
+      5.undefined
+      6.symbol => uniquness we use symbol
+
+            // non primitive
+      1. Array
+      2.Object      
+*/
+
+let uname = "Abhi";
+let age = "24";
+let isLoggedIn = false;
+let state = undefined; // undefined
+let temp = null ; // intentionally defining empty
+
+console.log(typeof uname); // string
+console.log(typeof age);   // number
+console.log(typeof isLoggedIn); // boolean
+console.log(typeof state); // undefined
+console.log(typeof temp); // object
+
+
+
