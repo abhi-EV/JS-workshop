2200f5c7e46f9cff2ddb412525a0090b0fd15c61
abhi-EV <abhishekabhishek7026@gmail.com>
2024-05-25 23:35:10 +0530

    arrayMethods

---
+++ 02-Basics/01-arrays.js
@@ -0,0 +1,32 @@
+let arr = [1,2,3,4,5,6];
+let myMovieLang = ["kannada","hindi","english"];
+const myArr = new Array("abhi","kailash","Girish");
+// console.log(arr[0]);
+// console.log(arr[1]);
+
+
+                    /* Array Methods */
+/*
+    1.length              16.indexOf
+    2.toString            17.lastIndexOf
+    3.at                  18.includes
+    4.join                19.find
+    5.pop                 20.findIndex
+    6.push                21.findLast
+    7.shift               22.findLastIndex   
+    8.unshift             23.sort
+    9.delete              24.reverse
+    10.concat             25.toSorted
+    11.copyWithin         26.toReversec
+    12.flat               27.forEach
+    13.splice             28.map
+    14.tospliced          29.filter
+    15.slice              30.flatMap
+    31.reduce             32.reduceRight
+    33.reduceRight        34.every
+    35.some               36.from
+    37.keys               38.entries
+    39.with               40.spread
+*/
+
+
