374be40e9983531b6d59841f50088a31fc0cdc6f
abhi-EV <abhishekabhishek7026@gmail.com>
2024-05-25 12:55:25 +0530

    variables

---
+++ 01-Basics/01-variables.js
@@ -0,0 +1,17 @@
+// console.log("hello world");
+
+// we can declare variables with diff keywrds such as ** let , var , const ** used based on requirement
+
+const acctId = 145263;
+let userName = "Abhi"
+var userEmail = "abhi@gmail.com";
+accountCity = "karnataka"; // we should not declare this type variable , its not good practice even though it not throw errorit reserves memoery 
+
+// acctId = 125; // we cant change const values reffer bellow code line
+// console.log(acctId);
+//      ** ***** ** ***** *** 
+// console.log([acctId, userName, userEmail, accountCity]);
+/*
+we should stop using var beacuse of issue in block and functional scope 
+*/
+
