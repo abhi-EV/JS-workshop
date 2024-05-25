cb5afcf456a082c6d9fb383a2320ab521b149fdb
abhi-EV <abhishekabhishek7026@gmail.com>
2024-05-25 23:13:51 +0530

    DateAndTime

---
+++ 01-Basics/06-DateAndTime.js
@@ -0,0 +1,33 @@
+/*              Date        */
+
+/*
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString("en-IN"));
+*/
+// in js month start from 0-jan,1-feb,2-mar,3-apr etc
+/*
+let myCreatedDate = new Date(2023,4,24,4,20);
+console.log(myCreatedDate);
+console.log(myCreatedDate.toString());
+console.log(myCreatedDate.toLocaleString());
+*/
+// let myCreatedDate = new Date("01-25-2024");
+// console.log(myCreatedDate.toLocaleString());
+// let TimeStamp = Date.now();
+// console.log(TimeStamp );
+// console.log(myCreatedDate.getTime()); // will get time in millisec
+
+// console.log(Math.floor(Date.now()/1000));
+
+// let newDate = new Date();
+// console.log(newDate.toLocaleString("default",{
+//     weekday:"long",
+//     day:"numeric",
+//     month:"long",
+// }));
+
+
+
