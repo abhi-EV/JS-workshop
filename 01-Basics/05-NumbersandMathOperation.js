e0f273be4bc2759ac8119025d736538409a9eae8
abhi-EV <abhishekabhishek7026@gmail.com>
2024-05-25 21:51:10 +0530

    Math and Num

---
+++ 01-Basics/05-NumbersandMathOperation.js
@@ -0,0 +1,22 @@
+/*let score = 250005;
+console.log(score);
+
+const balance = new Number(1111.4268);
+console.log(balance);
+console.log(balance.toString());
+console.log(balance.toFixed(1));
+
+const otherNum = 123.8625;
+console.log(otherNum.toPrecision(3));
+console.log(score.toLocaleString("en-IN"));*/
+
+//+++++++++++++++++   Maths   +++++++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-4));// turns to 4
+// console.log(Math.round(4.6));//5
+// console.log(Math.ceil(4.2))//5
+// console.log(Math.floor(4.2));//4 if we take 4.8 then also 4 we get
+// console.log(Math.min(4,3,5,7,8,2));//2
+// console.log(Math.max(4,8,9,5));//9
+
+// console.log(Math.floor(Math.random()*6 +1)); // will get numbers less than or equall to 6
